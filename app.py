6056df0076593c237e1231ddc649a7ae2ea93bf7
Breno Reis <reisb6774@gmail.com>
2025-10-12 10:29:30 -0300

    feat:Changing files

--- app.py
+++ app.py
@@ -508,5 +508,7 @@ def delete(cpf):
 
 if __name__ == "__main__":
     with app.app_context():
+        db.drop_all()
         db.create_all()
-    app.run(debug = True, host='127.0.0.1', port=5000)
+    port =  int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port = port)
