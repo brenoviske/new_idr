65b14f3a95c5a4d0a494c88a08c4f9e85e804f7c
Breno Reis <reisb6774@gmail.com>
2025-10-12 10:31:06 -0300

    feat:Removing drop all from the code to assure the tables will exist after changes

--- app.py
+++ app.py
@@ -508,7 +508,7 @@ def delete(cpf):
 
 if __name__ == "__main__":
     with app.app_context():
-        db.drop_all()
+        
         db.create_all()
     port =  int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0", port = port)
