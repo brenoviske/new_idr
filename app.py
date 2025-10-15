d5441a5a3d644dc1854b8bd366d0db9598b83ce9
Breno Reis <reisb6774@gmail.com>
2025-10-15 17:06:49 -0300

    feat: Collecting all the tables again

--- app.py
+++ app.py
@@ -311,7 +311,6 @@ def logout():
 #============================
 if __name__ == "__main__":
     with app.app_context():
-        db.drop_all()
         db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
